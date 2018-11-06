d885898d0ce2df57f7603c337acec0328f188b1a
moonlove95 <673817480@qq.com>
2018-11-07 00:09:53 +0800

    contact

---
+++ app/src/main/java/com/yigotone/app/ui/call/CallActivity.java
@@ -0,0 +1,35 @@
+package com.yigotone.app.ui.call;
+
+import com.yigotone.app.R;
+import com.yigotone.app.base.BaseActivity;
+import com.yigotone.app.base.BasePresenter;
+
+/**
+ * Created by ZMM on 2018/11/6  23:24.
+ */
+public class CallActivity extends BaseActivity {
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_call;
+    }
+
+    @Override
+    public BasePresenter initPresenter() {
+        return null;
+    }
+
+    @Override
+    public void initView() {
+
+    }
+
+    @Override
+    public void onFinish() {
+
+    }
+
+    @Override
+    public void onError(Throwable throwable) {
+
+    }
+}
